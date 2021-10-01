02c951801300a3efd99c2589167542d51bb71d76
isha912 <78261234+isha912@users.noreply.github.com>
2021-10-01 21:00:36 +0530

    Update 318A - Even Odds.cpp

--- Codeforces/Practice/C++/318A - Even Odds.cpp
+++ Codeforces/Practice/C++/318A - Even Odds.cpp
@@ -1,34 +1,22 @@
 #include <bits/stdc++.h>
- 
 using namespace std;
- 
-#define ll long long
-#define MOD 1000000007
- 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
- 
-     ll n,a;
-    cin>>n>>a;
- 
-    ll noOfOdds,noOfEvens;
-    if(n&1){
-        noOfOdds = n/2 + 1;
-        noOfEvens = n-noOfOdds; 
+
+
+
+int main()
+{
+    long long int n,k;
+    cin >> n >>k;
+
+    if(k <= (n+1)/2){
+        cout << (2*k)-1 << endl;
     }
-    else{
-        noOfEvens = (n)/2;
-        noOfOdds = n/2; 
+    else
+    {
+        k = k-((n+1) /2);
+        cout << 2*k << endl;
     }
- 
-    if(a <= noOfOdds){
-        cout<<1+(a-1)*2;
-    }
-    else {
-        a -= noOfOdds;  
-        cout<<2+(a-1)*2;
-    }
- 
+
     return 0;
 }
+
