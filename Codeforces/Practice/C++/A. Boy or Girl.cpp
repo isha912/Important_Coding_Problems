ef3d3b1c879b193dc4f91465fa2a56c9f73339f5
isha912 <78261234+isha912@users.noreply.github.com>
2021-10-01 21:07:42 +0530

    Update A. Boy or Girl.cpp

--- Codeforces/Practice/C++/A. Boy or Girl.cpp
+++ Codeforces/Practice/C++/A. Boy or Girl.cpp
@@ -1,18 +1,25 @@
-#define ll long long
 #include<bits/stdc++.h>
 using namespace std;
+
 int main()
 {
-	string s;
-	cin>>s;
-	set<char>p;
-	for(ll i=0;i<s.size();i++)
-	{
-		p.insert(s[i]);
-	}
-	if(p.size()%2==0)
-	cout<<"CHAT WITH HER!";
-	else
-	cout<<"IGNORE HIM!";
-}
+    string i;
+    cin>>i;
 
+    sort(i.begin(),i.end());
+    int l=i.length();
+    int gen=0;
+    
+	for(int x=1; x<l; x++)
+    {
+        if(i[x]!=i[x-1])
+            gen++;
+    }
+
+    if(gen&1)
+        cout<<"CHAT WITH HER!"<<endl;
+    else
+        cout<<"IGNORE HIM!"<<endl;
+
+    return 0;
+}
